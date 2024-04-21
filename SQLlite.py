6688b56d51676da80763d9e5a6cb181244f252c8
Darsh Vaidya <darshvaidya26@gmail.com>
2024-04-20 17:56:18 -0700

    database updated

--- SQLlite.py
+++ SQLlite.py
@@ -8,20 +8,23 @@ cursor = connection.cursor()
 
 #create table
 table_info = """
-Create table STUDENT(NAME VARCHAR(25), CLASS VARCHAR(25), SECTION VARCHAR(25));
+Create table STUDENT(NAME VARCHAR(25), CLASS VARCHAR(25), SECTION VARCHAR(25), MARKS INT);
 """
 
 cursor.execute(table_info)
 
 #insert records
 
-cursor.execute(''' INSERT Into STUDENT values('Darsh','AI','A')''')
-cursor.execute(''' INSERT Into STUDENT values('Aseem','ML','A')''')
-cursor.execute(''' INSERT Into STUDENT values('Devam','AI','A')''')
-cursor.execute(''' INSERT Into STUDENT values('Siddhant','ML','A')''')
+cursor.execute(''' INSERT Into STUDENT values('Darsh','AI','A',90)''')
+cursor.execute(''' INSERT Into STUDENT values('Aseem','ML','A',95)''')
+cursor.execute(''' INSERT Into STUDENT values('Devam','AI','B',85)''')
+cursor.execute(''' INSERT Into STUDENT values('Siddhant','ML','C',75)''')
 
 #Display records
 print("The inserted records:")
 data = cursor.execute('''SELECT * FROM STUDENT''')
 for row in data:
     print(row)
+
+connection.commit()
+connection.close()
